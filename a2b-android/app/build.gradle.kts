906e18bd53be0099e859fe404c97a8d74d80d3e9
Robert <bigbob1991@gmail.com>
2022-10-17 10:48:08 +0400

    polish and update

--- a2b-android/app/build.gradle.kts
+++ a2b-android/app/build.gradle.kts
@@ -113,7 +113,6 @@ dependencies {
 
   //google
   implementation("com.google.android.material:material:1.8.0-alpha01")
-  implementation("com.google.android.play:core:1.10.3")
   implementation("com.google.android.play:core-ktx:1.8.1")
   implementation("com.google.android.gms:play-services-location:21.0.0")
   implementation("com.google.code.gson:gson:2.8.9")
