d7790a0fdb07b17964d02234f61b9ac41844658f
Robert <bigbob1991@gmail.com>
2023-09-25 20:35:37 +0400

    new update

--- a2b-android/build.gradle.kts
+++ a2b-android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("com.android.application") version "8.1.0" apply false
-  id("com.android.library") version "8.1.0" apply false
+  id("com.android.application") version "8.1.1" apply false
+  id("com.android.library") version "8.1.1" apply false
   id("com.google.dagger.hilt.android") version "2.48" apply false
   id("com.google.gms.google-services") version "4.3.15" apply false
   id("com.google.firebase.crashlytics") version "2.9.5" apply false
